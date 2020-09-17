e67ecdb94e53cf8246021578dbddd117458659db
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-17 05:03:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,8 @@ def self.create_table
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
 
   def update
